4e88ad6888dbd608924daa9806debe4ccfe17168
trevorregister <trevor.register@gmail.com>
2024-08-28 10:49:07 -0400

    feat: placeholder text

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,10 +1,40 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
+      <v-col>
         <h1>About Me</h1>
-        <p>Welcome to my home page. This is where I will share my thoughts and ideas with the world.</p>
-        <v-img src="@/assets/me.jpg" height="200px" width="200px"/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="8">
+        <v-card>
+          <v-card-title>
+            <h2>My Story</h2>
+          </v-card-title>
+          <v-card-text>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed euismod, tortor nec tincidunt ultrices, mauris mauris ultrices nunc, nec tincidunt justo nunc id nunc. 
+                Nulla facilisi. Sed ut mauris auctor, lacinia nunc sed, aliquet nisl. Sed id nunc id nunc tincidunt tincidunt. 
+                Nulla facilisi. Sed ut mauris auctor, lacinia nunc sed, aliquet nisl.
+              </p>
+              <br>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed euismod, tortor nec tincidunt ultrices, mauris mauris ultrices nunc, nec tincidunt justo nunc id nunc. 
+                Nulla facilisi. Sed ut mauris auctor, lacinia nunc sed, aliquet nisl. Sed id nunc id nunc tincidunt tincidunt. 
+                Nulla facilisi. Sed ut mauris auctor, lacinia nunc sed, aliquet nisl.
+              </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-img 
+          src="@/assets/me.jpg" 
+          height="300px" 
+          width="300px"
+          rounded="circle"
+          />
       </v-col>
     </v-row>
   </v-container>
